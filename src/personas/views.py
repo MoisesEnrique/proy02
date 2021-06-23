b0a9565c8a9186bfb5e36077ab7cdb5134976b3f
MoisesEnrique <moises.mac@gmail.com>
2021-06-23 13:26:59 -0500

    DJango3 creando la vista y template test.html

--- src/personas/views.py
+++ src/personas/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+#importamos los modelos de persona
+from .models import Persona
+
 # Create your views here.
+# creamos la vista 
+def personaTestView(request):
+    obj = Persona.objects.get(id = 2)
+    
+    #definimos un diciionario para guardar los datos de la persona con id = 2
+    context = {
+        'nombre' : obj.nombres,
+        'edad' : obj.edad,
+    }
+    return render(request, 'personas/test.html', context)
+
