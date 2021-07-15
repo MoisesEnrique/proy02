422e0c3b6bb3fa4ad51b9083f2ae9e142ae9fbf9
MoisesEnrique <moises.mac@gmail.com>
2021-07-15 13:31:37 -0500

    Django 4 Colocando opciones core y clean al formulario

--- src/personas/forms.py
+++ src/personas/forms.py
@@ -14,9 +14,33 @@ class PersonaForm(forms.ModelForm):
             'donador',
         ]
 
+    def clean_nombres(self, *args, **kwargs):
+        name = self.cleaned_data.get('nombres')
+        if name.istitle():
+            return name
+        else:
+            raise forms.ValidationError('Primera letra en Mayuscula') 
+    
+
 class RawPersonaForm(forms.Form):
     #definimos los campos
-    nombres = forms.CharField()
-    apellidos = forms.CharField()
-    edad = forms.IntegerField()
+    nombres = forms.CharField(label="Tu Nombre")
+    apellidos = forms.CharField(
+        widget = forms.Textarea(
+            attrs={
+                'placeholder':'Ingrese un Apellido',
+                'id':'apellidoID',
+                'class':'special',
+                'cold':'10',
+            }
+        )
+    )		
+    edad = forms.IntegerField(initial = 20, min_value=20, max_value=30)
     donador = forms.BooleanField(required=False)
+
+    def clean_nombres(self, *args, **kwargs):
+        name = self.cleaned_data.get('nombres')
+        if name.istitle():
+            return name
+        else:
+            raise forms.ValidationError('Primera letra en Mayuscula') 
